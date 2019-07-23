bbd8e859033d921e771918880f27a77fe439da2c
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2019-07-23 12:55:20 +1000

    Add ZD badges, prefill reviewer field

--- views/opsdash.js
+++ views/opsdash.js
@@ -26,15 +26,18 @@ $(document).on('knack-scene-render.scene_713', function(event, scene) {
   })
 
   // Get an array of job names that have been scheduled for install
+  // 'Install Booked' table showing callouts
   let scheduledInstalls = JSON.parse(JSON.stringify(Knack.views.view_2237.model.data.models))
   scheduledInstalls = scheduledInstalls.map(callout => callout.field_928_raw[0].identifier)
 
-  // Get an array of job names that have been measured without issues
+  // Get an array of job names that have been installed without issues
+  // 'Installed' table of call outs
   let completeInstalls = JSON.parse(JSON.stringify(Knack.views.view_2238.model.data.models))
-  completeInstalls = completeInstalls.filter(callout => callout.field_1542.indexOf('No Issues') > -1)
+  completeInstalls = completeInstalls.filter(callout => callout.field_1542.indexOf('Follow Up') === -1)
   completeInstalls = completeInstalls.map(callout => callout.field_928_raw[0].identifier)
 
   // Get an array of all jobs currently in 'book for install' or 'install booked' status
+  // 'Book For Install' table of Jobs
   let jobsToInstall = JSON.parse(JSON.stringify(Knack.views.view_2231.model.data.models))
 
   // Get an array of jobs that are installed or booked for install according to callouts
@@ -44,6 +47,12 @@ $(document).on('knack-scene-render.scene_713', function(event, scene) {
   jobsToHide.forEach(job => {
     document.getElementById(job.id).remove()
   })
+
+  // Get an array of job names that have been installed without issues
+  // 'Installed' table of call outs
+  let jobsRequiringFollowUp = JSON.parse(JSON.stringify(Knack.views.view_2238.model.data.models))
+  jobsRequiringFollowUp = completeInstalls.filter(callout => callout.field_1542.indexOf('Follow Up') > -1)
+
 
   removeUnusedTableGroups() // removing items from tables can orphan a grouping heading
   hideEmptyTables(scene)
@@ -83,3 +92,35 @@ function removeUnusedTableGroups() {
     }
   }
 }
+
+// CUSTOM OPPORTUNITIES
+// Add open/pending badges to opportunities in 'To Quote' status
+$(document).on('knack-view-render.view_1038', function(event, view, data) {
+  data.forEach(record => {
+    if (record['field_1606'].indexOf('Open') > -1) {
+      let $openQuote = $('#' + record.id)
+      $openQuote.find('.col-1').prepend('<div class="ZDBadge ZDOpen">O</div>')
+    }
+    if (record['field_1606'].indexOf('Pending') > -1) {
+      let $pendingQuote = $('#' + record.id)
+      $pendingQuote.find('.col-1').prepend('<div class="ZDBadge ZDPending">P</div>')
+    }
+  })
+})
+
+// Send For Review form loaded
+// Update the review field to the sales person
+$(document).on('knack-view-render.view_1024', async function(event, view, data) {
+  Knack.showSpinner()
+  try {
+    let opportunity = await getRecordPromise(view.scene.object, view.scene.scene_id)
+    if (opportunity.field_1274_raw.length > 0) {
+      $reviewer = $('#' + view.key + '-field_830')
+      let salesPerson = await getRecordPromise('object_82', opportunity.field_1274_raw[0].id)
+      $reviewer.html(`<option value='${salesPerson.id}'>${salesPerson.field_956}</option>`).trigger('liszt:updated')
+    }
+  } catch (error) {
+    updateLog(`Send for review error: \`\`\`${error.message}\n${error.stack}\`\`\``)
+  }
+  Knack.hideSpinner()
+})
