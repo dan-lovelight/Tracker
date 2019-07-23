9edcc7d39ff244a23b79eae01a08516911118cd6
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2019-07-23 12:54:52 +1000

    bug fix

--- objects/opportunities.js
+++ objects/opportunities.js
@@ -1,34 +1,36 @@
 $(document).on('knack-view-render.any', function(event, view, data) {
   try {
-    let changeLogger
+    let opportunity
     let user = Knack.getUserAttributes()
 
     if (view.source) {
       if (view.source.object) {
-        opportunity = new KnackObject(view.source.object, view)
-        changeLogger.onChange(globalOpportunityChange)
+        if (view.source.object === 'object_17') {
+          opportunity = new KnackObject(view.source.object, view)
+          opportunity.onChange(globalOpportunityChange)
+        }
       }
     }
 
-    function globalOpportunityChange(view, record, action, fields, recordBefore, changes){
+    function globalOpportunityChange(view, record, action, fields, recordBefore, changes) {
       let data = {}
       let isStatusUpdated = changes.includes('field_127')
       let isQuoteStatusUpdated = changes.includes('field_1606')
 
       // Record the status change date
-      if(isStatusUpdated) {
+      if (isStatusUpdated) {
         data.field_1609 = {
           "date": moment().format("DD/MM/YYYY"),
         }
       }
       // Record the date the quoted status changed (eg Open/Pending)
-      if(isQuoteStatusUpdated) {
+      if (isQuoteStatusUpdated) {
         data.field_1610 = {
           "date": moment().format("DD/MM/YYYY"),
         }
       }
       // Update the record if a change has taken place
-      if(isQuoteStatusUpdated || isStatusUpdated){
+      if (isQuoteStatusUpdated || isStatusUpdated) {
         opportunity.update(record.id, data)
       }
     }
