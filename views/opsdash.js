029c6f77e67ceb51607d2c55e7e90ad18db142a6
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2019-07-24 11:54:05 +1000

    enhanced logic

--- views/opsdash.js
+++ views/opsdash.js
@@ -4,55 +4,132 @@
 $(document).on('knack-scene-render.scene_713', function(event, scene) {
   // The code below relies on multiple views being loaded and so needs to run on scene render
 
-  // Get an array of job names scheduled for measure
-  let scheduledMeasures = JSON.parse(JSON.stringify(Knack.views.view_2234.model.data.models))
-  scheduledMeasures = scheduledMeasures.map(callout => callout.field_928_raw[0].identifier)
+  // ------------- Book for Measure Logic --------------//
 
-  // Get an array of job names that have been measured without issues
-  let completeMeasures = JSON.parse(JSON.stringify(Knack.views.view_2235.model.data.models))
-  completeMeasures = completeMeasures.filter(callout => callout.field_1542.indexOf('No Issues') > -1)
-  completeMeasures = completeMeasures.map(callout => callout.field_928_raw[0].identifier)
+  const scheduledMeasures = JSON.parse(JSON.stringify(Knack.views.view_2234.model.data.models))
+  const completedMeasures = JSON.parse(JSON.stringify(Knack.views.view_2235.model.data.models))
+  const jobsReadyToMeasure = JSON.parse(JSON.stringify(Knack.views.view_2220.model.data.models))
 
-  // Get an array of all jobs currently in 'book for measure' or 'measure booked' status
-  let jobsToMeasure = JSON.parse(JSON.stringify(Knack.views.view_2220.model.data.models))
+  // It's possible to have more than one callout in the table for a single job
+  let uniqueCompletedMeasures = new Set()
+  let duplicateCompletedMeasures = new Set()
 
-  // Get an array of jobs that are measured or booked for measure, according to callouts
-  let jobsToHide = jobsToMeasure.filter(job => {
-    return scheduledMeasures.includes(job.field_296_raw) || completeMeasures.includes(job.field_296_raw)
+  // Loop through all completed measures, identify dupes, and add to correct array
+  for(let i = 0; i < completedMeasures.length; i++){
+    let thisJobId = completedMeasures[i].field_928_raw[0].id
+    let measuresForThisJob = completedMeasures.filter(callout => thisJobId === callout.field_928_raw[0].id)
+    // If there were multiples, add to the appropriate array
+    if(measuresForThisJob.length>1){
+      let d1 = Date.parse(measuresForThisJob[0].field_939.date)
+      let d2 = Date.parse(measuresForThisJob[1].field_939.date)
+      if(d1>d2){
+        uniqueCompletedMeasures.add(measuresForThisJob[0])
+        duplicateCompletedMeasures.add(measuresForThisJob[1])
+      } else {
+        uniqueCompletedMeasures.add(measuresForThisJob[1])
+        duplicateCompletedMeasures.add(measuresForThisJob[0])
+      }
+    } else { // Only one, add to unique array
+      uniqueCompletedMeasures.add(measuresForThisJob[0])
+    }
+  }
+
+  uniqueCompletedMeasures = Array.from(uniqueCompletedMeasures)
+  duplicateCompletedMeasures = Array.from(duplicateCompletedMeasures)
+
+  const successfullyMeasured = uniqueCompletedMeasures.filter(callout => callout.field_1542.indexOf('No Issues') > -1)
+  const unsuccessfullyMeasured = uniqueCompletedMeasures.filter(callout => callout.field_1542.indexOf('Follow') > -1)
+  const unreportedMeasures = uniqueCompletedMeasures.filter(callout => callout.field_1542.length === 0)
+  const successfullyAndUnreportedMeasures = successfullyMeasured.concat(unreportedMeasures)
+
+  const scheduledForMeasureJobIds = scheduledMeasures.map(callout => callout.field_928_raw[0].id)
+  const successfullyMeasuredJobIds = successfullyAndUnreportedMeasures.map(callout => callout.field_928_raw[0].id)
+  const jobsIdsBookedForOrAlreadyMeasured = scheduledForMeasureJobIds.concat(successfullyMeasuredJobIds)
+
+  const jobsToHideFromBookForMeasureTable = jobsReadyToMeasure.filter(job => jobsIdsBookedForOrAlreadyMeasured.includes(job.id))
+  jobsToHideFromBookForMeasureTable.forEach(job => document.getElementById(job.id).remove())
+
+  const duplicateCallOutIdsToHideFromMeasuredTable = duplicateCompletedMeasures.map(callout => callout.id)
+  duplicateCallOutIdsToHideFromMeasuredTable.forEach(callout => document.getElementById(callout).remove())
+
+  // Add notification to jobs in book for status that have unsuccessful installs
+  unsuccessfullyMeasured.forEach(callout =>{
+    let $job = $('#'+callout.field_928_raw[0].id)
+    if ($job.length>0){
+        let notification = `Measure on ${callout.field_939} requires follow up`
+        $job.find('.col-1').append(`<div class="ZDBadge ZDNotification">${notification}</div>`)
+    }
   })
-  // Remove jobs
-  jobsToHide.forEach(job => {
-    document.getElementById(job.id).remove()
+
+  // -------------End Book for Measure Logic --------------//
+
+  // -------------Book for Install Logic --------------//
+
+  const scheduledInstalls = JSON.parse(JSON.stringify(Knack.views.view_2237.model.data.models))
+  const completedInstalls = JSON.parse(JSON.stringify(Knack.views.view_2238.model.data.models))
+  const jobsReadyToInstall = JSON.parse(JSON.stringify(Knack.views.view_2231.model.data.models)).concat(JSON.parse(JSON.stringify(Knack.views.view_2230.model.data.models))) //Book for and return visit tables
+
+  let uniqueCompletedInstalls = new Set()
+  let duplicateCompletedInstalls = new Set()
+
+  // Loop through all completed installs, identify dupes, and add to correct array
+  for(let i = 0; i < completedInstalls.length; i++){
+    let thisJobId = completedInstalls[i].field_928_raw[0].id
+    let installsForThisJob = completedInstalls.filter(callout => thisJobId === callout.field_928_raw[0].id)
+    // If there were multiples, add to the appropriate array
+    if(installsForThisJob.length>1){
+      let d1 = moment(installsForThisJob[0].field_939,"DD/MM/YYYY")
+      let d2 = moment(installsForThisJob[1].field_939,"DD/MM/YYYY")
+      if(d1>d2){
+        uniqueCompletedInstalls.add(installsForThisJob[0])
+        duplicateCompletedInstalls.add(installsForThisJob[1])
+      } else {
+        uniqueCompletedInstalls.add(installsForThisJob[1])
+        duplicateCompletedInstalls.add(installsForThisJob[0])
+      }
+    } else { // Only one, add to unique array
+      uniqueCompletedInstalls.add(installsForThisJob[0])
+    }
+  }
+
+  uniqueCompletedInstalls = Array.from(uniqueCompletedInstalls)
+  duplicateCompletedInstalls = Array.from(duplicateCompletedInstalls)
+
+  // Get completed installs by their report status
+  const successfullyInstalled = uniqueCompletedInstalls.filter(callout => callout.field_1542.indexOf('No Issues') > -1)
+  const unsuccessfullyInstalled = uniqueCompletedInstalls.filter(callout => callout.field_1542.indexOf('Follow') > -1)
+  const unreportedInstalls = uniqueCompletedInstalls.filter(callout => callout.field_1542.length === 0)
+  const successfullyAndUnreportedInstalls = successfullyInstalled.concat(unreportedInstalls)
+
+  // Get ids of jobs that are scheduled for install, or succesfully installed, based on callouts
+  const scheduledForInstallJobIds = scheduledInstalls.map(callout => callout.field_928_raw[0].id)
+  const successfullyInstalledJobIds = successfullyAndUnreportedInstalls.map(callout => callout.field_928_raw[0].id)
+  const jobsIdsBookedForOrAlreadyInstalled = scheduledForInstallJobIds.concat(successfullyInstalledJobIds)
+
+  // Hide jobs in the book for table that have bookings or succsfully installs
+  const jobsToHideFromBookForInstallTable = jobsReadyToInstall.filter(job => jobsIdsBookedForOrAlreadyInstalled.includes(job.id))
+  jobsToHideFromBookForInstallTable.forEach(job => document.getElementById(job.id).remove())
+
+  // Hide duplicate callouts from the installed table
+  const duplicateCallOutIdsToHideFromInstallTable = duplicateCompletedInstalls.map(callout => callout.id)
+  duplicateCallOutIdsToHideFromInstallTable.forEach(callout => document.getElementById(callout).remove())
+
+  // Hide completed installs if there's another one already scheduled
+  uniqueCompletedInstalls.forEach(callout => {
+    if(scheduledForInstallJobIds.includes(callout.field_928_raw[0].id)){
+      let calloutRow = document.getElementById(callout.id)
+      if(calloutRow) calloutRow.remove()
+    }
   })
 
-  // Get an array of job names that have been scheduled for install
-  // 'Install Booked' table showing callouts
-  let scheduledInstalls = JSON.parse(JSON.stringify(Knack.views.view_2237.model.data.models))
-  scheduledInstalls = scheduledInstalls.map(callout => callout.field_928_raw[0].identifier)
-
-  // Get an array of job names that have been installed without issues
-  // 'Installed' table of call outs
-  let completeInstalls = JSON.parse(JSON.stringify(Knack.views.view_2238.model.data.models))
-  completeInstalls = completeInstalls.filter(callout => callout.field_1542.indexOf('Follow Up') === -1)
-  completeInstalls = completeInstalls.map(callout => callout.field_928_raw[0].identifier)
-
-  // Get an array of all jobs currently in 'book for install' or 'install booked' status
-  // 'Book For Install' table of Jobs
-  let jobsToInstall = JSON.parse(JSON.stringify(Knack.views.view_2231.model.data.models))
-
-  // Get an array of jobs that are installed or booked for install according to callouts
-  jobsToHide = jobsToInstall.filter(job => {
-    return scheduledInstalls.includes(job.field_296_raw) || completeInstalls.includes(job.field_296_raw)
+  // Add notification to jobs in book for status that have unsuccessful installs
+  unsuccessfullyInstalled.forEach(callout =>{
+    let $job = $('#'+callout.field_928_raw[0].id)
+    if ($job.length>0){
+        let notification = `Install on ${callout.field_939} requires follow up`
+        $job.find('.col-1').append(`<div class="ZDBadge ZDNotification">${notification}</div>`)
+    }
   })
-  jobsToHide.forEach(job => {
-    document.getElementById(job.id).remove()
-  })
-
-  // Get an array of job names that have been installed without issues
-  // 'Installed' table of call outs
-  let jobsRequiringFollowUp = JSON.parse(JSON.stringify(Knack.views.view_2238.model.data.models))
-  jobsRequiringFollowUp = completeInstalls.filter(callout => callout.field_1542.indexOf('Follow Up') > -1)
-
 
   removeUnusedTableGroups() // removing items from tables can orphan a grouping heading
   hideEmptyTables(scene)
@@ -64,7 +141,8 @@ $(document).on('knack-scene-render.scene_713', function(event, scene) {
 $(document).on('knack-view-render.view_2235', function(event, view, data) {
   data.forEach(record => {
     if (record['field_928.field_245'].indexOf('Measure') === -1) {
-      document.getElementById(record.id).remove()
+      let calloutRow = document.getElementById(record.id)
+      if(calloutRow) calloutRow.remove()
     }
   })
 })
@@ -75,7 +153,8 @@ $(document).on('knack-view-render.view_2235', function(event, view, data) {
 $(document).on('knack-view-render.view_2238', function(event, view, data) {
   data.forEach(record => {
     if (record['field_928.field_245'].indexOf('Complete') > -1) {
-      document.getElementById(record.id).remove()
+      let calloutRow = document.getElementById(record.id)
+      if(calloutRow) calloutRow.remove()
     }
   })
 })
