27dd65a25c1581b46b10d170292c8045e786caa2
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2019-09-24 09:53:14 +1000

    added options menu

--- views/jobdetails.js
+++ views/jobdetails.js
@@ -14,9 +14,42 @@ $(document).on('knack-scene-render.scene_52', function(event, scene) {
     }
   })
 
-  collapseTables(scene)
+  // collapseTables(scene)
+  formatHeading('view_2339')
+  // attacheOptionsMenu('view_2339')
 
 })
+
+function formatHeading(viewKey){
+  // Move the status into the heading
+  $('.job-status-wrapper').appendTo($('#' + viewKey + ' .kn-detail-body'))
+  // Remove the second table column
+  $('#' + viewKey + ' .kn-details-group > div:nth-child(2)').remove()
+}
+
+function attacheOptionsMenu(viewKey){
+  let menu = `
+  <div id="jobs-menu">
+    <div class="kn-navigation-bar pull-right">
+      <nav class="tabs is-boxed">
+        <ul>
+          <li class="kn-dropdown-menu">
+            <a>
+              <span>Options&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+              <span class="kn-dropdown-icon fa fa-caret-down"></span>
+            </a>
+            <ul class="kn-dropdown-menu-list" style="min-width: 81px;">
+              <li><a href="#" data-kn-slug="#"><span><img src="https://freeicons.io/laravel/public/uploads/icons/png/18487936531552562368-256.png">&nbsp;&nbsp;View Client In Zendesk</span></a></li>
+              <li><a href="#custom" data-kn-slug="#custom"><span><i class="fa fa-home"></i>&nbsp;&nbsp;Custom</span></a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>`
+
+  $('#' + viewKey + ' .kn-details-group').append(menu)
+}
 
 function collapseTables(scene) {
   // keep track if we've found a table yet
@@ -68,47 +101,3 @@ function collapseTables(scene) {
     }
   })
 }
-
-
-// `<div id="jobs-menu" style="padding-top:1px">
-//   <div class="kn-navigation-bar pull-right">
-//     <nav class="tabs is-boxed">
-//       <ul>
-//         <li class="kn-dropdown-menu">
-//           <a>
-//             <span>Options</span>
-//             <span class="kn-dropdown-icon fa fa-caret-down"></span>
-//           </a>
-//           <ul id="job-menu-options" class="kn-dropdown-menu-list" style="min-width: 81px;">
-//             <li><a href="#" data-kn-slug="#"><span><i class="fa fa-circle-o-notch"></i>&nbsp;&nbsp;Option 1</span></a></li>
-//           </ul>
-//         </li>
-//       </ul>
-//     </nav>
-//   </div>
-// </div>`
-
-// <div id="jobs-menu" style="padding-top:1px">
-//   <div class="kn-navigation-bar pull-right">
-//     <nav class="tabs is-boxed">
-//       <ul>
-//         <li class="kn-dropdown-menu">
-//           <a>
-//             <span>Options&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-//             <span class="kn-dropdown-icon fa fa-caret-down"></span>
-//           </a>
-//           <ul class="kn-dropdown-menu-list" style="min-width: 81px;">
-//             <li><a href="#" data-kn-slug="#"><span><img src="https://freeicons.io/laravel/public/uploads/icons/png/18487936531552562368-256.png">&nbsp;&nbsp;View Client In Zendesk</span></a></li>
-//           <li><a href="#custom" data-kn-slug="#custom"><span><i class="fa fa-home"></i>&nbsp;&nbsp;Custom</span></a></li></ul>
-//         </li>
-//       </ul>
-//     </nav>
-//   </div>
-// </div>
-
-// <span class="job-status-wrapper"><div id="job-status">
-//   <div id="job-status">
-//     <div style="vertical-align: middle;display: table-cell"> <img src="https://img.icons8.com/ios/2x/ruler.png" width="30" height="30"> </div>
-//     <div style="vertical-align: middle;display: table-cell"> &nbsp;Measured </div>
-//   </div>
-// </span>
