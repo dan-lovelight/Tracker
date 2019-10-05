d482a1e1e11ce71ad596d14072b5ed5be4d15b0c
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2019-10-05 20:39:11 +1000

    full refactor

--- objects/orders.js
+++ objects/orders.js
@@ -1,101 +1,112 @@
-const orderCreated = [
-  'knack-record-create.view_1139', //Add estimate
-  'knack-record-create.view_96', //Add order
-  'knack-record-create.view_1019', //Record Stock Usage
-  'knack-record-create.view_1000', //Create Related Order
-]
+async function processOrderChange({record:order, action, changes, previous, view}) {
 
-const orderUpdated = [
-  'knack-form-submit.view_1140', //Edit Estimate
-  'knack-form-submit.view_223', //Edit Order
-  'knack-form-submit.view_997', //Change Order Status
-  'knack-form-submit.view_1333', //Update Storage Bay
-  'knack-form-submit.view_1334', //Update Carton Number
-  'knack-form-submit.view_1353', //Warehouse Change Order Status
-  'knack-cell-update.view_97', //Job details table
-  'knack-cell-update.view_1087', //Search Orders
-  'knack-cell-update.view_998', //Related orders table in order details
-  'knack-cell-update.view_1287', //Stocktake table
-  'knack-cell-update.view_1332', //Deliver manifest table
-  'knack-cell-update.view_1335', //Recently received table
-]
+  try {
+    // Get data to update the order
+    let statusUpdates = getOrderStatusUpdates(order, previous, changes)
+    let stocktakeUpdates = getOrderStocktakeUpdates(order)
 
-// Order is created
-$(document).on(orderCreated.join(' '), function(event, view, record) {
-  if (record.field_1446_raw[0].id === '5c09553e2e148d3a56e05b6a') { // FabCon
-    let zapData = {
-      'job': record.field_10.length > 0 ? record.field_10_raw[0].identifier : '',
-      'product': record.field_11.length > 0 ? record.field_11_raw[0].identifier : '',
-      'quantity': record.field_17,
-      'metres': record.field_1617,
-      'fabric': `${record.field_935.length>0 ? record.field_935_raw[0].identifier : ''} - ${record.field_909.length>0 ? record.field_909_raw[0].identifier : ''}`,
-      'tapeSupplied': record.field_1618,
-      'dateOrderd': record.field_1619,
-      'datePromised': record.field_1620,
-      'contact': record.field_53.length > 0 ? record.field_53_raw[0].identifier : '',
+    // Consolidate the data
+    let updateData = Object.assign({}, statusUpdates, stocktakeUpdates)
+
+    // Update the job
+    if (!$.isEmptyObject(updateData)) {
+      let ordersObj = new KnackObject(objects.orders)
+      order = await ordersObj.update(order.id, updateData)
     }
-    triggerZap('oo1i5ut', zapData, 'new fabcon order');
+
+    // These downstream actions use the updated order object
+    // Important as the status may have changed (eg bay entered)
+    handleOrderNotifications(order, previous, changes)
+    handleOrderPortalStatusChange(order, previous, changes)
+
+  } catch (err) {
+    if(!Sentry) throw err
+    Sentry.captureException(err)
   }
-  processOrderChanges(record);
-});
-
-// Order is updated
-$(document).on(orderUpdated.join(' '), function(event, view, record) {
-  processOrderChanges(record);
-});
-
-async function processOrderChanges(record) {
-
-  let trackStatusChange = [
-    ['field_442', 'field_443']
-  ]
-
-  let status = record.field_442_raw["0"].identifier
-
-  // Sometimes an order's bay is entered without the status being updated
-  // This looks for these instances and updates the status to 'In Warehouse'
-  // Placing this before the status change check triggers downstream updates
-  if (status.includes("Ordered") && record.field_90.length > 0) record.field_442 = ['59086d0d86d2272d7a9805db']
-
-  let today = moment().format("DD/MM/YYYY")
-  let now = moment().format("DD/MM/YYYY hh:mma")
-  let isStatusUpdated = isObjectUpdated(record, trackStatusChange)
-  let isJustReceived = isStatusUpdated && status.indexOf('Warehouse') > -1
-  let isStockTakeToday = record.field_1001.length > 0 && record.field_1001_raw.date_formatted === today
-
-  // Gather data for the updated order
-  let updatedOrder = {}
-
-  if (isStatusUpdated) {
-    updatedOrder = copyFieldsToNewObject(record, trackStatusChange) // Reset change tracking fields
-    updatedOrder.field_264 = now // Status update date
-  }
-
-  //Has it just been recieved?
-  if (isJustReceived) {
-    updatedOrder.field_22 = today; //set to order received date to today
-    sendOrderReceivedNotifications(record)
-    if (record.field_591 === 'Apartments') {
-      let portalData = {
-        jobId: record.field_10_raw[0].id,
-        newPortalState: 'order_received',
-        date: ''
-      }
-      changeStatusInPortal(portalData)
-    }
-  }
-
-  //Was this record stocktake today?
-  if (isStockTakeToday) {
-    updatedOrder.field_1000 = record.field_1000_raw // Set the stocktake bay to the current bay
-  }
-
-  //Update the record
-  updateRecordPromise('object_5', record.id, updatedOrder)
 
 }
 
-async function sendOrderReceivedNotifications(record) {
+function isOrderStatusUpdated(changes) {
+  if (changes.includes('field_442')) return true
+  return false
+}
+
+function isOrderStatusOrdered(order) {
+  if (order.field_442_raw[0].identifier.includes("Ordered")) return true
+  return false
+}
+
+function isOrderStatusWarehouse(order) {
+  if (order.field_442_raw[0].identifier.includes("In Warehouse")) return true
+  return false
+}
+
+function isOrderStockTakeToday(order){
+  let today = moment().format("DD/MM/YYYY")
+  if( order.field_1001.length > 0 && order.field_1001_raw.date_formatted === today) return true
+  return false
+}
+
+function isOrderBayJustEntered(changes,previous){
+  if(changes.includes('field_90') && previous.field_90.length === 0) return true
+  return false
+}
+
+function isOrderJustReceived(order, previous, changes){
+  let isStatusUpdated = isOrderStatusUpdated(changes)
+  let isInWarehouse = isOrderStatusWarehouse(order)
+  let isOrdered = isOrderStatusOrdered(order)
+  let isBayJustEntered = isOrderBayJustEntered(changes, previous)
+  if(isStatusUpdated && isInWarehouse) return true
+  if(isBayJustEntered && isOrdered) return true
+  return false
+}
+
+function getOrderStatusUpdates(order, previous, changes){
+  let data = {}
+
+  let isStatusUpdated = isOrderStatusUpdated(changes)
+  let isJustReceived = isOrderJustReceived(order,previous,changes)
+  let isBayJustEntered = isOrderBayJustEntered(changes,previous)
+  let isOrdered = isOrderStatusOrdered(order)
+
+  // Update dates of status change and order received
+  if(isStatusUpdated) data.field_264 = moment().format("DD/MM/YYYY hh:mma")
+  if(isJustReceived) data.field_22 = moment().format("DD/MM/YYYY")
+
+  // If ordered status but just got a bay, update to in warehouse status
+  if(isOrdered && isBayJustEntered) data.field_442 = ['59086d0d86d2272d7a9805db']
+
+  return data
+}
+
+function getOrderStocktakeUpdates(order){
+  let data = {}
+  let isStockTakeToday = isOrderStockTakeToday(order)
+  if(isStockTakeToday) data.field_1000 = order.field_90_raw
+  return data
+}
+
+function handleOrderPortalStatusChange(order, previous, changes) {
+
+  let isJustReceived = isOrderJustReceived(order, previous, changes)
+
+  if(!isJustReceived) return
+  if (!order.field_591 === 'Apartments')  return
+
+  let portalData = {
+    jobId: order.field_10_raw[0].id,
+    newPortalState: 'order_received',
+    date: ''
+  }
+
+  changeStatusInPortal(portalData)
+  }
+
+async function handleOrderNotifications(order,previous,changes) {
+
+  let isJustReceived = isOrderJustReceived(order, previous, changes)
+  if(!isJustReceived) return
 
   let zapData = {}
 
@@ -109,19 +120,23 @@ async function sendOrderReceivedNotifications(record) {
   zapData.notes = record.field_18;
   zapData.status = status;
 
-  let job = await getRecordPromise('object_3', record.field_10_raw[0].id)
+  let jobsObj = new KnackObject(objects.jobs)
+  let job = await jobsObj.get(record.field_10_raw[0].id)
+
   zapData.jobStatus = job.field_245_raw[0].identifier
   zapData.jobName = job.field_296
 
   // Get salesperson details
   if (job.field_1276.length > 0) {
-    let salesperson = await getRecordPromise('object_82', job.field_1276_raw[0].id)
+    let salesObj = new KnackObject(objects.salespeople)
+    let salesperson = await salesObj.get(job.field_1276_raw[0].id)
     zapData.salesEmail = salesperson.field_957_raw.email
   }
 
   // Get salesperson details
   if (job.field_1277.length > 0) {
-    let opsperson = await getRecordPromise('object_68', job.field_1277_raw[0].id)
+    let opsObj = new KnackObject(objects.opspeople)
+    let opsperson = await opsObj.get(job.field_1277_raw[0].id)
     zapData.opsEmail = opsperson.field_814_raw.email
   }
 
