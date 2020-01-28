2cca5a5e70296fd88e63b251fde72306ad02cd6e
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2020-01-28 11:24:17 +1100

    bug fix

--- views/businesses.js
+++ views/businesses.js
@@ -1,4 +1,4 @@
-$(document).on('knack-view-render.view_2490', async function(event, view, data) {
+$(document).on('knack-view-render.view_2492', async function(event, view, data) {
   pimpContactField(view, 'field_1564')
   pimpContactField(view, 'field_1727')
 })
